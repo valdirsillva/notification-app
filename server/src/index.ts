63c586fa27cc7f3ab7ead97c9e932d3dbe4f86ad
valdirsillva <valdirpiresba@gmail.com>
2025-08-23 12:04:56 -0300

    modificando porta

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import { server } from "./server";
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
